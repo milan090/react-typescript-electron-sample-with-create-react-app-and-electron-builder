c5eb2a4afa59e01a1796fa100cf8179cfede8773
milan090 <milanmuhammed1@gmail.com>
2021-06-03 17:10:16 +0530

    yarn lock created

--- electron/main.ts
+++ electron/main.ts
@@ -1,20 +1,23 @@
-import { app, BrowserWindow } from 'electron';
-import * as path from 'path';
-import * as isDev from 'electron-is-dev';
-import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as isDev from "electron-is-dev";
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,
+} from "electron-devtools-installer";
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    backgroundColor: "#FFF",
     webPreferences: {
       // contextIsolation: false,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
   if (isDev) {
-    win.loadURL('http://localhost:3000/index.html');
+    win.loadURL("http://localhost:3000/index.html");
   } else {
     // 'build/index.html'
     win.loadURL(`file://${__dirname}/../index.html`);
@@ -23,15 +26,17 @@ function createWindow() {
   // Hot Reloading
   if (isDev) {
     // 'node_modules/.bin/electronPath'
-    require('electron-reload')(__dirname, {
-      electron: path.join(__dirname,
-        '..',
-        '..',
-        'node_modules',
-        '.bin',
-        'electron' + (process.platform === "win32" ? ".cmd" : "")),
+    require("electron-reload")(__dirname, {
+      electron: path.join(
+        __dirname,
+        "..",
+        "..",
+        "node_modules",
+        ".bin",
+        "electron" + (process.platform === "win32" ? ".cmd" : "")
+      ),
       forceHardReset: true,
-      hardResetMethod: 'exit'
+      hardResetMethod: "exit",
     });
   }
 
@@ -44,18 +49,18 @@ app.whenReady().then(() => {
   // DevTools
   installExtension(REACT_DEVELOPER_TOOLS)
     .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
+    .catch((err) => console.log("An error occurred: ", err));
 
   createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
       app.quit();
     }
   });
